29e9c54844c55ee894ff6c98eecef0bd50e6bc15
amanat361 <amanat361@gmail.com>
2023-06-13 14:12:10 -0700

    Added maze script

--- Spawner.cs
+++ Spawner.cs
@@ -4,24 +4,69 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject floor;
     public GameObject wall;
+    private int mazeSize;
+    private Vector3 wallSize, floorSize;
     private Vector3 spawnPos;
-    // Start is called before the first frame update
+    private int rotation, modifier;
+    float x, y, z, dx, dz;
+    List<int[]> maze;
+
     void Start()
     {
-        InvokeRepeating("SpawnWall", 0, 1);
+        InitializeMaze();
     }
 
-    void SpawnWall() {
-        spawnPos.x = Random.Range(-50, 50);
-        spawnPos.y = 1.5f;
-        spawnPos.z = Random.Range(-50, 50);
-        Instantiate(wall, spawnPos, Quaternion.identity, transform);
-    }
-
-    // Update is called once per frame
-    void Update()
+    void UpdateCoordinates()
     {
-        
+        x = maze[rotation % maze.Count][1] * wallSize.x;
+        z = maze[rotation % maze.Count][0] * wallSize.x;
+        dx = maze[rotation % maze.Count][1] * wallSize.z / 2;
+        dz = maze[rotation % maze.Count][0] * wallSize.z / 2;
+    }
+
+    void InitializeMaze()
+    {
+        // get wall size and maze size
+        wallSize = wall.transform.localScale;
+        floorSize = floor.transform.localScale;
+        mazeSize = (int)floorSize.x / (int)wallSize.x;
+
+        // set maze directions
+        maze = new List<int[]>() { new int[] { 0, 1 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { -1, 0 } };
+
+        // set initial rotation to 0
+        rotation = 0;
+        modifier = 0;
+
+        // set initial spawn position
+        x = (floorSize.x / -2f) + (wallSize.x / 2f);
+        z = (floorSize.z / -2f) + (wallSize.z / 2f);
+        y = (floorSize.y /  2f) + (wallSize.y / 2f);
+        spawnPos = new Vector3(x, y, z);
+
+        // update x, z, dx, dz
+        UpdateCoordinates();
+
+        // spawn walls
+        for (int i = 1; rotation < mazeSize; i++)
+        {
+            // instantiate wall
+            GameObject newWall = Instantiate(wall, spawnPos, Quaternion.identity, transform);
+            newWall.transform.Rotate(0, rotation * 90, 0);
+
+            // if we've reached the end of a row, update spawn position
+            if ((i+modifier) % mazeSize == 0)
+            {
+                spawnPos += new Vector3(x/2 - dx, 0, z/2 - dz);
+                rotation++;
+                modifier += rotation;
+                UpdateCoordinates();
+                spawnPos += new Vector3(x/2 - dx, 0, z/2 - dz);
+            } else {
+                spawnPos += new Vector3(x, 0, z);
+            }
+        }
     }
 }
